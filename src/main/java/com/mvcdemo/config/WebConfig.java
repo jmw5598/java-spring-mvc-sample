fbd07cb56376cbb5b0f9adde494b62401787dd80
jmw <jmw5598@gmail.com>
2016-05-17 14:51:38 -0400

    Added internalized messages

--- src/main/java/com/mvcdemo/config/WebConfig.java
+++ src/main/java/com/mvcdemo/config/WebConfig.java
@@ -1,12 +1,23 @@
 package com.mvcdemo.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
@@ -31,6 +42,47 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		
 		configurer.enable();
+		
+	}
+	
+	// Beans to setup message internalization to target appropriate language.
+	@Bean
+	public MessageSource messageSource() {
+		
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("resources/messages/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+		
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setDefaultLocale(new Locale("es"));
+		resolver.setCookieName("lang");
+		resolver.setCookieMaxAge(4800);
+		return resolver;
+		
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("language");
+		
+		registry.addInterceptor(interceptor);
+		
+	}
+	
+	@Override
+	public Validator getValidator() {
+		
+		LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(messageSource());
+		return validator;
 		
 	}
 	
