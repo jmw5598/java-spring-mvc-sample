5d97d82a09b91bc84e4d3a6c9a5e77f8e932e068
jmw <jmw5598@gmail.com>
2016-05-18 14:36:44 -0400

    Added mappings for /login and /logout

--- src/main/java/com/mvcdemo/web/controller/HomeController.java
+++ src/main/java/com/mvcdemo/web/controller/HomeController.java
@@ -2,19 +2,47 @@ package com.mvcdemo.web.controller;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping({"/", "/home"})
+@RequestMapping({"/"})
 public class HomeController {
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(value={"/"}, method=RequestMethod.GET)
+	public String login(Model model) {
+		
+		return "login";
+		
+	}
+	
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		//Checks to see if user is logged in, it true, logs the user out.
+		if(auth != null)
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		
+		return "redirect:/login?logout";
+	}
+	
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
 		return "home";
 	
 	}
+	
 }
