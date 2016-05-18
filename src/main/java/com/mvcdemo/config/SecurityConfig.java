859553fb95675025dfd980ccbc06fafb718b9277
jmw <jmw5598@gmail.com>
2016-05-18 14:36:11 -0400

    Added Spring Security config

---
+++ src/main/java/com/mvcdemo/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.mvcdemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.permitAll()
+		.and()
+		.logout()
+			.logoutSuccessUrl("/")
+			.permitAll();
+		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		web
+			.ignoring()
+				.antMatchers("/resources/**");
+		
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth
+			.inMemoryAuthentication()
+				.withUser("jason").password("1112632").roles("USER");
+		
+	}
+	
+}
--- src/main/java/com/mvcdemo/config/WebConfig.java
+++ src/main/java/com/mvcdemo/config/WebConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -85,5 +86,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		return validator;
 		
 	}
+	
 	
 }
